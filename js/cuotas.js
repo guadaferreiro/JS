95df39f9a306e8b75f00d7ac3783b17b745ae68f
Maria Guadalupe <guadalupeferreiro1@gmail.com>
2022-07-06 18:58:16 -0300

    cambio en cuotas.js

--- js/cuotas.js
+++ js/cuotas.js
@@ -4,13 +4,9 @@ let intereses = 0.15;
 precioTotal = parseInt (prompt ("Ingrese el precio "));
 
 if (precioTotal >= "0") {
-
     cuotas = precioTotal * intereses;
-
     console.log ("La cuota es de " + cuotas);
 
     precioFinal = precioTotal * intereses + precioTotal;
-
     console.log ("El precio final es de " + precioFinal);
-
 } 
